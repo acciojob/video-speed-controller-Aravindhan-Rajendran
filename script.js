40ba12eb18f863b4ba194497aa1dbe182c07d5b1
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-30 13:17:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function getplayspeed(){
+	  let playbackRateInput = document.querySelector('.controls .playbackRate'); // replace '.playbackRate' with the correct class or id of your input
+    alert(playbackRateInput.value);
+}
+
+function setplayspeed(){
+    let video = document.querySelector('video'); // replace 'video' with the correct selector if necessary
+    video.playbackRate = 0.5;
+}
